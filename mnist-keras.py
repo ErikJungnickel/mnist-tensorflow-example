b0b3f500d2382a8cb5638bdfe3fc558f85b761a9
Erik <erik.jungnickel@bertelsmann.de>
2017-11-17 13:17:54 +0100

    refactoring

--- mnist-keras.py
+++ mnist-keras.py
@@ -5,51 +5,65 @@ from keras.layers import Conv2D, MaxPool2D, Dense, Activation, Flatten
 from keras.optimizers import Adam
 from keras.datasets import mnist
 
-# get the mnist data set and divide it into train & test data (x_ is the actual image data, y_ are the labels)
-(x_train, y_train), (x_test, y_test) = mnist.load_data()
 
-# images are 28x28 pixels with 1 channel (greyscale image = 1 channel, rbg image would have 3 channels)
-x_train = x_train.reshape(x_train.shape[0], 28, 28, 1)
-x_test = x_test.reshape(x_test.shape[0], 28, 28, 1)
-input_shape = (28, 28, 1)
+def preprocessImageData():
+    # get the mnist data set and divide it into train & test data (x_ is the actual image data, y_ are the labels)
+    (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
-# image data is presented as value between 0 and 255 -> bring it to a 0 - 1 value
-x_train = x_train.astype('float32')
-x_test = x_test.astype('float32')
-x_train /= 255
-x_test /= 255
+    # images are 28x28 pixels with 1 channel (greyscale image = 1 channel, rbg image would have 3 channels)
+    x_train = x_train.reshape(x_train.shape[0], 28, 28, 1)
+    x_test = x_test.reshape(x_test.shape[0], 28, 28, 1)
 
-# preprocess the label data (there are 10 possible labels - 0 to 9)
-y_train = keras.utils.to_categorical(y_train, 10)
-y_test = keras.utils.to_categorical(y_test, 10)
+    input_shape = (28, 28, 1)
 
-# keras sequential model
-model = Sequential()
+    # image data is presented as value between 0 and 255 -> bring it to a 0 - 1 value
+    x_train = x_train.astype('float32')
+    x_test = x_test.astype('float32')
+    x_train /= 255
+    x_test /= 255
 
-# convolution layer with 32 features, the first layer needs to know the input shape
-model.add(Conv2D(32, 5, activation='relu', input_shape=input_shape))
+    # preprocess the label data (there are 10 possible labels - 0 to 9)
+    y_train = keras.utils.to_categorical(y_train, 10)
+    y_test = keras.utils.to_categorical(y_test, 10)
 
-# pooling layer
-model.add(MaxPool2D())
+    return input_shape, x_train, y_train, x_test, y_test
 
-# convolution layer with 64 features
-model.add(Conv2D(64, 5, activation='relu'))
 
-# pooling layer
-model.add(MaxPool2D())
+def convolution():
+    # keras sequential model
+    model = Sequential()
 
-# flatten the input
-model.add(Flatten())
+    # convolution layer with 32 features, the first layer needs to know the input shape    
+    model.add(Conv2D(32, 5, activation='relu', input_shape=input_shape))
 
-# fully connected layer, 10 outputs (the probability for each digit), the sum of all probabilites is equal to 1 (softmax)
-model.add(Dense(10, activation='softmax'))
+    # pooling layer
+    model.add(MaxPool2D())
 
-# weight optimization using Adam and categorical_crossentropy as loss funciton
-model.compile(optimizer='Adam', loss='categorical_crossentropy',
-              metrics=['accuracy'])
+    # convolution layer with 64 features
+    model.add(Conv2D(64, 5, activation='relu'))
+
+    # pooling layer
+    model.add(MaxPool2D())
+
+    # flatten the input
+    model.add(Flatten())
+
+    # fully connected layer, 10 outputs (the probability for each digit), the sum of all probabilites is equal to 1 (softmax)
+    model.add(Dense(10, activation='softmax'))
+
+    # weight optimization using Adam and categorical_crossentropy as loss funciton
+    model.compile(optimizer='Adam', loss='categorical_crossentropy',
+                  metrics=['accuracy'])
+
+    return model
+
+
+input_shape, x_train, y_train, x_test, y_test = preprocessImageData()
+
+model = convolution()
 
 # train the model
-model.fit(x_train, y_train, epochs=1, batch_size=32)
+model.fit(x_train, y_train, epochs=10, batch_size=32)
 
 # evaluate our model with the test mnist set
 score = model.evaluate(x_test, y_test)
